2fe7dd0577796aa9db546738c413c05a079e24c6
Manvi Arora <130341603+manvi3007@users.noreply.github.com>
2026-01-19 16:30:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Swap The Numbers/swap-the-numbers.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+
+########### Write your code below ###############
+
+# Write Code to Swap
+
+########### Write your code above ###############
+a,b=b,a
+print(a, b)
